b9e7c27c240ca8b09ba6130238ff2d95aa5a3f7b
Hüseyin <huseyint428@gmail.com>
2026-04-16 14:26:11 +0300

    first commit

---
+++ app/haberler/1/page.js
@@ -0,0 +1,5 @@
+export default function News1(){
+  return(
+    <h1>Birinci Haber</h1>
+  )
+}
---
+++ app/hakkimizda/page.js
@@ -0,0 +1,8 @@
+export default function About(){
+  return(
+    <div>
+      <h1>Hakkımızda Sayfam</h1>
+      <h2>Hüseyin Tunç</h2>
+    </div>
+  )
+}
